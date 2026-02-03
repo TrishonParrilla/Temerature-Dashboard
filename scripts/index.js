5dc534633c6ab99a8ff388927cbe4fb26375502b
TrishonParrilla <trishonp1@gmail.com>
2026-02-03 00:41:54 -0700

    Add conversion table functionality and style updates

--- scripts/index.js
+++ scripts/index.js
@@ -12,3 +12,17 @@ function convertToCelsius() {
     conversionOutput = document.getElementById("convert-result")
     conversionOutput.innerHTML=(`${userValue}`)
 }
+
+function conversionTable() {
+    celsiusInsert = document.getElementById("conversionTable");
+    fahrenheitInsert = document.getElementById("conversionTable");
+    for(i=0; i<110; i+=10) {
+        celsius = i;
+        fahrenheit = (i * 9/5 + 32);
+        fahrenheitInsert.innerHTML+= 
+        `<tr>
+        <td>${celsius}</td>
+        <td>${fahrenheit}</td>
+        </tr>`;
+    }
+}
