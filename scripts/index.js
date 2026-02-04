bb871fd3287c3c6ef5223dc81db73cdc5e6c6927
TrishonParrilla <trishonp1@gmail.com>
2026-02-03 20:04:45 -0700

    Enhance temperature conversion logic and update thermometer styles for Celsius input

--- scripts/index.js
+++ scripts/index.js
@@ -27,18 +27,48 @@ function convertToFahrenheit() {
 
 function convertToCelsius() {
     userValue = document.getElementById("temp-converter-temp").value;
-    userValue = (userValue - 32) * 5/9;
-    conversionOutput = document.getElementById("convert-result")
-    conversionOutput.innerHTML=(`${userValue}`)
+    userValue = (userValue - 32 ) * 5/9;
+    conversionOutput = document.getElementById("convert-result");
+    conversionOutput.innerHTML=(`${userValue}`);
+    const thermometerStyle = document.getElementById("thermometer");
+        thermometerStyle.classList.remove("thermometer-hot");
+        thermometerStyle.classList.remove("thermometer-hottest");
+        thermometerStyle.classList.remove("thermometer-mid");
+        if(userValue>70 && userValue < 100) {
+            thermometerStyle.classList.remove("thermometer-hot");
+            thermometerStyle.classList.add("thermometer-hot");
+            
+        }
+        else if (userValue > 100) {
+            thermometerStyle.classList.remove("thermometer-hottest");
+            thermometerStyle.classList.add("thermometer-hottest");
+            
+        }        
+        else if (userValue <= 70 ) {
+            thermometerStyle.classList.remove("thermometer-mid");
+            thermometerStyle.classList.add("thermometer-mid");
+            
+        }
+    
 }
 
+
 function conversionTable() {
-    celsiusInsert = document.getElementById("conversionTable");
-    fahrenheitInsert = document.getElementById("conversionTable");
-    for(i=0; i<110; i+=10) {
-        celsius = i;
-        fahrenheit = (i * 9/5 + 32);
-        fahrenheitInsert.innerHTML+= 
+        let fahrenheitInsert = document.getElementById("conversionTable");
+        if (fahrenheitInsert.innerHTML.trim() !== "") {
+        fahrenheitInsert.innerHTML = "";
+        return
+    }
+        fahrenheitInsert.innerHTML = `<tr>
+        <th>Celsius</th>
+        <th>Fahrenheit</th>
+        </tr>`;
+    
+    
+        for(i=0; i<110; i+=10) {
+        let celsius = i;
+        let fahrenheit = (i * 9/5 + 32);
+        fahrenheitInsert.innerHTML += 
         `<tr>
         <td>${celsius}</td>
         <td>${fahrenheit}</td>
